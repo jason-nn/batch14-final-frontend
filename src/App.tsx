339e7bdf5d5d08a7c6ff59ee3b7d297deaa9c2a0
Jason Ho <jason.ho@obf.ateneo.edu>
2022-03-31 23:49:45 +0800

    Add axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
 import React from 'react';
+// import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import axios from 'axios';
 
 import Alerts from './pages/Alerts';
 import Home from './pages/Home';
 import Purchases from './pages/Purchases';
 
 const App: React.FC = () => {
+  axios.defaults.baseURL = 'http://localhost:4000';
+
+  // useEffect(() => {
+  //   axios({
+  //     method: 'post',
+  //     url: 'users/sign_in',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     data: {
+  //       user: {
+  //         email: 'jason@hodlr.com',
+  //         password: 'password',
+  //       },
+  //     },
+  //   })
+  //     .then((response) => {
+  //       const data = response.data;
+
+  //       const id = data.id;
+  //       const token = data.token;
+
+  //       axios({
+  //         method: 'post',
+  //         url: 'api/v1/alerts',
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //         data: {
+  //           user_id: id,
+  //           cryptocurrency_id: 1,
+  //           price: 40000,
+  //           operator: 'lower',
+  //         },
+  //       })
+  //         .then((response) => console.log(response))
+  //         .catch((error) => console.log(error));
+  //     })
+  //     .catch((error) => console.log(error));
+  // }, []);
+
   return (
     <BrowserRouter>
       <Routes>
