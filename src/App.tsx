1eac0bc692bc31cfeedeca98c55bdf71751f7c06
Jason Ho <jason.ho@obf.ateneo.edu>
2022-03-31 23:03:49 +0800

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-const App = () => {
-  return <div>App</div>;
+import Alerts from './pages/Alerts';
+import Home from './pages/Home';
+import Purchases from './pages/Purchases';
+
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/purchases" element={<Purchases />} />
+        <Route path="/alerts" element={<Alerts />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/Purchases.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Purchases: React.FC = () => {
+  return <div>Purchases</div>;
+};
+
+export default Purchases;
