b912c34ce1bdfdb795506f0074b7bc583405a3c6
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-03 00:06:00 +0800

    Change inputs in sign in page to validated inputs

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-
 import { Link } from 'react-router-dom';
+import { z } from 'zod';
 
 import AuthOuterContainer from 'components/auth/AuthOuterContainer';
 import AuthInnerContainer from 'components/auth/AuthInnerContainer';
@@ -8,11 +8,15 @@ import AuthHeader from 'components/auth/AuthHeader';
 import AuthFooter from 'components/auth/AuthFooter';
 
 import Form from 'components/shared/Form';
-import BaseInput from 'components/shared/inputs/BaseInput';
+import ValidatedBaseInput from 'components/shared/inputs/ValidatedBaseInput';
 
 const SignIn: React.FC = () => {
   const [emailValue, setEmailValue] = useState<string>('');
+  const [isEmailValueValid, setIsEmailValueValid] = useState<boolean>(false);
+
   const [passwordValue, setPasswordValue] = useState<string>('');
+  const [isPasswordValueValid, setIsPasswordValueValid] =
+    useState<boolean>(false);
 
   return (
     <AuthOuterContainer>
@@ -24,20 +28,30 @@ const SignIn: React.FC = () => {
           onSubmit={() => {
             console.log('Call create session API');
           }}
-          disabled={true}
+          disabled={!(isEmailValueValid && isPasswordValueValid)}
           buttonText="Sign in"
         >
-          <BaseInput
+          <ValidatedBaseInput
             label="Email"
             type="text"
             value={emailValue}
             setValue={setEmailValue}
+            setIsValid={setIsEmailValueValid}
+            validators={z
+              .string()
+              .min(1, { message: 'This field is required' })
+              .email({ message: 'Must be a valid email' })}
           />
-          <BaseInput
+          <ValidatedBaseInput
             label="Password"
             type="password"
             value={passwordValue}
             setValue={setPasswordValue}
+            setIsValid={setIsPasswordValueValid}
+            validators={z
+              .string()
+              .min(1, { message: 'This field is required' })
+              .min(8, { message: 'Must be at least 8 characters' })}
           />
         </Form>
 
