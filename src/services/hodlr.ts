a92984a5d1fec0f08ce24f34e5295f46ef69b13f
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-03 13:34:02 +0800

    Add hodlr api service

---
+++ src/services/hodlr.ts
@@ -0,0 +1,145 @@
+import axios from 'axios';
+
+interface User {
+  email: string;
+  password: string;
+}
+
+interface Purchase {
+  cryptocurrencyId: number;
+  price: number;
+  quantity: number;
+}
+
+interface Alert {
+  cryptocurrencyId: number;
+  price: number;
+  operator: 'higher' | 'lower';
+}
+
+const hodlr = {
+  signIn: (user: User) => {
+    return axios({
+      method: 'post',
+      url: 'api/v1/sessions',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        user: {
+          email: user.email.toLowerCase(),
+          password: user.password,
+        },
+      },
+    });
+  },
+
+  signUp: (user: User) => {
+    return axios({
+      method: 'post',
+      url: 'api/v1/registrations',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: {
+        user: {
+          email: user.email.toLowerCase(),
+          password: user.password,
+        },
+      },
+    });
+  },
+
+  cryptocurrencies: (token: string) => {
+    return axios({
+      method: 'get',
+      url: 'api/v1/cryptocurrencies',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  cryptocurrency: (token: string, symbol: string) => {
+    return axios({
+      method: 'get',
+      url: `api/v1/cryptocurrencies/${symbol.toLowerCase()}`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  purchases: (token: string) => {
+    return axios({
+      method: 'get',
+      url: `api/v1/purchases`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  createPurchase: (token: string, purchase: Purchase) => {
+    return axios({
+      method: 'post',
+      url: `api/v1/purchases`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        purchase: {
+          cryptocurrency_id: purchase.cryptocurrencyId,
+          price: purchase.price,
+          quantity: purchase.quantity,
+        },
+      },
+    });
+  },
+
+  alerts: (token: string) => {
+    return axios({
+      method: 'get',
+      url: `api/v1/alerts`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+
+  createAlert: (token: string, alert: Alert) => {
+    return axios({
+      method: 'post',
+      url: `api/v1/alerts`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      data: {
+        alert: {
+          cryptocurrency_id: alert.cryptocurrencyId,
+          price: alert.price,
+          operator: alert.operator,
+        },
+      },
+    });
+  },
+
+  destroyAlert: (token: string, id: number) => {
+    return axios({
+      method: 'delete',
+      url: `api/v1/alerts/${id}`,
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+};
+
+export default hodlr;
