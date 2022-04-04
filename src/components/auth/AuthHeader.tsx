b8b68553fac5216262512ce0948ef00e45f3470a
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-04 23:14:30 +0800

    Change top margin in auth header

--- src/components/auth/AuthHeader.tsx
+++ src/components/auth/AuthHeader.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import Logo from 'components/shared/Logo';
 
 const AuthHeaderContainer = styled.div`
-  margin: 10px 0px;
+  margin: 30px 0px 10px 0px;
 `;
 
 const AuthHeader: React.FC = () => {
