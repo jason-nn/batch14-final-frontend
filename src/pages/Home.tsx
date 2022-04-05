33ad637b28d73c9fa354121be024c91588517709
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-05 23:22:19 +0800

    Add groundwork for purchases page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,39 @@
+import { motion } from 'framer-motion';
 import React from 'react';
+import styled from 'styled-components';
 
+import BodyContainer from 'components/shared/BodyContainer';
+import Header from 'components/shared/Header';
 import Navbar from 'components/shared/Navbar';
 import SignedIn from 'components/shared/SignedIn';
+import PrimaryButton from 'components/shared/buttons/PrimaryButton';
+
+const AlignRight = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`;
 
 const Home: React.FC = () => {
   return (
     <SignedIn>
       <Navbar selected="home" />
-      <div>Home</div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
+        <BodyContainer>
+          <Header>DCA and Current Price</Header>
+          <Header>Portfolio</Header>
+          <AlignRight>
+            <PrimaryButton
+              buttonText="New purchase"
+              disabled={false}
+              loading={false}
+            />
+          </AlignRight>
+        </BodyContainer>
+      </motion.div>
     </SignedIn>
   );
 };
