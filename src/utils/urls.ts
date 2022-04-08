250118fbca8c024268a628ab289f8895e62591e8
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-09 00:58:10 +0800

    Change urls to prod

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,8 @@
 const urls = {
-  backend: 'http://localhost:4000',
-  websocket: 'ws://localhost:4000/cable',
+  // backend: 'http://localhost:4000',
+  // websocket: 'ws://localhost:4000/cable',
+  backend: 'https://hodlrbyjason.herokuapp.com',
+  websocket: 'ws://hodlrbyjason.herokuapp.com/cable',
 };
 
 export default urls;
