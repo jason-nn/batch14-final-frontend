e90f469ecfd73eff5e12a96b018a324882c7076d
Jason Ho <jason.ho@obf.ateneo.edu>
2022-04-09 01:26:42 +0800

    Edit urls

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,6 @@
 const urls = {
   // backend: 'http://localhost:4000',
-  // websocket: 'wss://localhost:4000/cable',
+  // websocket: 'ws://localhost:4000/cable',
   backend: 'https://hodlrbyjason.herokuapp.com',
   websocket: 'wss://hodlrbyjason.herokuapp.com/cable',
 };
